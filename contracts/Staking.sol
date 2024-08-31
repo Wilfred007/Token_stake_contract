9d3836f75c7bc4f32d771ba3e38f9cb97b9cb712
Wilfred007 <adzerwilfred007@gmail.com>
2024-09-01 00:52:36 +0100

    functions

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+
+error Staking_TransferFailed();
 contract Staking {
     IERC20 public s_stakingToken;
     //we map users address to how much they have staked 
     mapping(address => uint256) public s_balances; 
+
+    uint256 public s_totalSupply;
 
 //This code states that at deployment the token address should be passed in
     constructor(address stakingToken) {
@@ -16,8 +20,17 @@ contract Staking {
         //keep track of ho much token we have
         //transfer tokens to contract
 
-        //Here we keep track of users balances by increasing the balance with he amount they have staked
+        //Here we keep track of users balances by increasing the balance with he amount they have staked, here we use the mapping to track the address of each user
         s_balances[msg.sender] = s_balances[msg.sender] + amount;
+
+        //this code tracks the total supply of tokens that have been sent to the contract
+        s_totalSupply = s_totalSupply + amount;
+
+
+        //This code makes use of the transferFrom function in ERC20 to transfer tokens to our contract account
+       bool success =  s_stakingToken.transferFrom(msg.sender, address(this), amount);
+        require(success, "Failed");
+
     }
 }
 
